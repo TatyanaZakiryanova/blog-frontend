0aa2203c2f05d80e36be0956403a54e3ef5ef914
TatyanaZakiryanova <elenastrell@yandex.ru>
2025-04-13 12:50:18 +0300

    add baseurl

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://blog-backend-vrgr.onrender.com',
 });
 
 instance.interceptors.request.use((config) => {
